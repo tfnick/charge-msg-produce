ab7453a1e8a3d2d5ddf8d43acbb501e331a9fa53
Administrator <admin@cafintech.com>
2019-01-19 15:53:32 +0800

    debug

--- kong/plugins/charge-msg-produce/paths.lua
+++ kong/plugins/charge-msg-produce/paths.lua
@@ -5,10 +5,10 @@ local ipairs = ipairs
 local function create_path_code_table(conf)
   local all_path_codes = {}
   for idx, value in ipairs(conf.path_prodcode_mappings) do
+    ngx.log(ngx.NOTICE," singl key is ", idx)
+    ngx.log(ngx.NOTICE," singl value is ", value)
     local single_path_code = types.single_path_prod_table(value)
     if not single_path_code then
-      return nil, "invalid single_path_prod_table value: " .. value
-    else
       for k, v in ipairs(single_path_code)  do
         all_path_codes[k] = v
       end
--- kong/plugins/charge-msg-produce/types.lua
+++ kong/plugins/charge-msg-produce/types.lua
@@ -18,6 +18,7 @@ end
 --- Parses `path:code` string into a `{path:code}` table.
 function _M.single_path_prod_table(string)
   local m = re_match(string, path_prod_mappings_regex, "jo")
+  ngx.log(ngx.NOTICE," m ", m[1]..m[2])
   if not m then
     return nil, "invalid path_prod_mappings value: " .. string
   end
